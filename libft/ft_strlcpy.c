1c2633273af4035fbc9327c6e78ad251d371ba91
Kornpong Methawanitpong <kmethawa@z3t4c2.42bangkok.com>
2022-02-24 01:15:18 +0700

    ft_strlcpy

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmethawa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 00:29:31 by kmethawa          #+#    #+#             */
+/*   Updated: 2022/02/24 01:14:52 by kmethawa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srcsize;
+
+	srcsize = ft_strlen(src);
+	if (srcsize + 1 < dstsize)
+		ft_memcpy(dst, src, srcsize + 1);
+	else if (dstsize)
+	{
+		ft_memcpy(dst, src, dstsize - 1);
+		dst[dstsize - 1] = '\0';
+	}
+	return (srcsize);
+}
+/*
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srcsize;
+
+	srcsize = ft_strlen(src);
+	if (srcsize < dstsize)
+		ft_strncpy(dst, src, srcsize);
+	else if (dstsize)
+	{
+		ft_strncpy(dst, src, dstsize);
+		dst[dstsize - 1] = '\0';
+	}
+	return (srcsize);
+}
+*/
+
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: kmethawa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 23:21:32 by kmethawa          #+#    #+#             */
-/*   Updated: 2022/02/23 22:03:05 by kmethawa         ###   ########.fr       */
+/*   Updated: 2022/02/24 00:28:37 by kmethawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
