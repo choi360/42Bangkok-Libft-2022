32a4d5ea37e1194f4fdb0b4e5f7a122826a4bf8b
Kornpong Methawanitpong <kornpong@Kornpongs-MacBook-Air.local>
2022-02-24 19:55:39 +0700

    minor update

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -65,3 +65,26 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+char	*s;
+	size_t	len_dst;
+	size_t	res;
+	size_t	len_src;
+	size_t	i;
+
+	s = (char *)src;
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(s);
+	res = 0;
+	i = 0;
+	if (size > len_dst)
+		res = len_src + len_dst;
+	else
+		res = len_src + size;
+	while (s[i] && (len_dst + 1) < size)
+	{
+		dst[len_dst] = s[i];
+		len_dst++;
+		i++;
+	}
+	dst[len_dst] = '\0';
+	return (res);
